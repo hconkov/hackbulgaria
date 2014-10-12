1aa297df93a27cc76585d55ed648f447a3323c3f
hconkov <z1rkel4o@gmail.com>
2014-10-13 00:29:59 +0300

    Create 2.8.py

---
+++ week0/2.8.py
@@ -0,0 +1,8 @@
+from math import fractions
+def sortfractions(fractions):
+    sortedfraction = []
+    for n in fractions:
+        fraction = fraction.Fraction(n)
+        if fraction >= 
+            fract.append(n)
+        elif 
