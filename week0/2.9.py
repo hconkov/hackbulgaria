bf07bed7c1744662029a9b6ee38388114f49d608
hconkov <z1rkel4o@gmail.com>
2014-10-13 08:53:41 +0300

    Create 2.9.py

---
+++ week0/2.9.py
@@ -0,0 +1,13 @@
+def fibolist(lista, listb, n):
+    listc = []
+    if n == 1:
+        return (lista)
+    elif n == 2:
+        return (listb)
+    else:
+        for i in range(3, n+1):
+            listc = lista + listb
+            lista = listb
+            listb = listc
+    return(listc)
+print(fibolist([1, 2], [1, 3], 3))
