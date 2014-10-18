2bb356ffe2508f9293a0175dfdd4711278f8a2ba
hconkov <z1rkel4o@gmail.com>
2014-10-18 16:57:11 +0300

    19.10.1

--- week1/oop/01.py
+++ week1/oop/01.py
@@ -1,8 +1,28 @@
-class cashdesk(object):
+class Cashdesk(object):
 
-    def __init__(self, money):
-        self.money = {100: 0, 50: 0, 20: 0, 10: , 5: 0, 2: 0, 1: 0}
+    def __init__(self):
+        self.money = {100: 0, 50: 0, 20: 0, 10: 0, 5: 0, 2: 0, 1: 0}
 
-    def takemoney(self):
+    def takemoney(self, takenmoney):
+        for key in takenmoney:
+            self.money[key] += takenmoney[key]
+
+    def total(self):
+        total = 0
         for key in self.money:
-            
+            total += self.money[key] * key
+        return(total)
+
+    def can_withdraw_money(self, amount_of_money):
+        print self.total
+        if amount_of_money > self.total:
+            return(False)
+        else:
+            return(True)
+
+
+cashdesk = Cashdesk()
+cashdesk.takemoney({1: 2, 50: 1, 20: 1})
+cashdesk.total()
+print(cashdesk.total())
+print(cashdesk.can_withdraw_money(74))
